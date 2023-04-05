880565f3b0d35be0510dd4817e442ed87bfc1b6b
aeuhim <ericmichaelbedanaconstantino@gmail.com>
2023-04-05 13:14:36 +0800

    Delete all

--- pages/api/post.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-    const { title, post } = JSON.parse(req.body);
-
-    // Then save the post data to a database
-    console.log(req.body)
-    res.status(200).json({ message: `${title}\n${post}` });
-}
